6b8083a23ef738e2ae8ba92e73612e60887f19e1
Jesse Kempf <jesse.kempf@opower.com>
2013-07-12 12:45:00 -0700

    Add comparator option to graphite check-data plugin

--- plugins/graphite/check-data.rb
+++ plugins/graphite/check-data.rb
@@ -15,44 +15,40 @@ class CheckGraphiteData < Sensu::Plugin::Check::CLI
   option :target,
     :description => 'Graphite data target',
     :short => '-t TARGET',
-    :long => '--target TARGET',
-    :required => true
+    :long => '--target TARGET'
 
   option :server,
     :description => 'Server host and port',
     :short => '-s SERVER:PORT',
-    :long => '--server SERVER:PORT',
-    :required => true
+    :long => '--server SERVER:PORT'
 
   option :warning,
-    :description => 'Generate warning if given value is above received value',
+    :description => 'Generate warning if given value exceeds received value',
     :short => '-w VALUE',
     :long => '--warn VALUE',
     :proc => proc{|arg| arg.to_f }
 
   option :critical,
-    :description => 'Generate critical if given value is above received value',
+    :description => 'Generate critical if given value exceeds received value',
     :short => '-c VALUE',
     :long => '--critical VALUE',
     :proc => proc{|arg| arg.to_f }
 
-  option :reset_on_decrease,
-    :description => 'Send OK if value has decreased on any values within END-INTERVAL to END',
+  option :reset_on_change,
+    :description => 'Send OK if value has beneficially changed on any values within END-INTERVAL to END',
     :short => '-r INTERVAL',
     :long => '--reset INTERVAL',
-    :proc => proc{|arg| arg.to_i }
+    :proc => proc{|arg| arg.to_i}
 
   option :name,
     :description => 'Name used in responses',
     :short => '-n NAME',
-    :long => '--name NAME',
-    :default => "graphite check"
+    :long => '--name NAME'
 
   option :allowed_graphite_age,
     :description => 'Allowed number of seconds since last data update (default: 60 seconds)',
     :short => '-a SECONDS',
     :long => '--age SECONDS',
-    :default => 60,
     :proc => proc{|arg| arg.to_i }
 
   option :hostname_sub,
@@ -60,56 +56,61 @@ class CheckGraphiteData < Sensu::Plugin::Check::CLI
     :short => '-s CHARACTER',
     :long => '--host-sub CHARACTER'
 
-  option :from,
-    :description => 'Get samples starting from FROM (default: -10mins)',
-    :short => '-f FROM',
-    :long => '--from FROM',
-    :default => "-10mins"
-
-  option :help,
-    :description => 'Show this message',
-    :short => '-h',
-    :long => '--help'
+  option :comparator,
+    :description => "Comparison to use when checking values. default(: gt) {gt|lt}",
+    :short => '-C COMPARISON',
+    :long  => '--comparison COMPARISON',
+    :proc => proc { |arg| {'gt' => :>, 'lt' => :<}[arg] },
+    :default => :>
 
   # Run checks
   def run
-    if config[:help]
-      puts opt_parser if config[:help]
-      exit
-    end
-    retreive_data || check_age || check(:critical) || check(:warning) || ok("#{name} value okay")
+    check_config || retreive_data || check_age || check(:critical) || check(:warning) || ok("#{name} value okay")
   end
 
   # name used in responses
   def name
-    base = config[:name]
+    base = config[:name] || "graphite check"
     @formatted ? "#{base} (#{@formatted})" : base
+  end
+
+  # check we have required configuration
+  def check_config
+    [:server, :target].detect do |key|
+      unless(config[key])
+        unknown "No graphite #{key} provided"
+      end
+    end
+
+    unless config[:comparator].is_a?(Symbol)
+      unknown "Unknown comparator #{config[:comparator]}. Valid options: {gt, lt}."
+    end
   end
 
   # Check the age of the data being processed
   def check_age
-    if (Time.now.to_i - @end) > config[:allowed_graphite_age]
-      critical "Graphite data age is past allowed threshold (#{config[:allowed_graphite_age]} seconds)"
+    if((Time.now.to_i - @end) > (config[:allowed_graphite_age] || 60))
+      critical "Graphite data age is past allowed threshold (#{config[:allowed_graphite_age] || 60} seconds)"
     end
   end
 
   # grab data from graphite
   def retreive_data
-    unless @raw_data
+    unless(@raw_data)
       begin
-        handle = open("http://#{config[:server]}/render?format=json&target=#{formatted_target}&from=#{config[:from]}")
+        handle = open("http://#{config[:server]}/render?format=json&target=#{formatted_target}")
         @raw_data = JSON.parse(handle.gets).first
-        @raw_data['datapoints'].delete_if{|v| v.first == nil}
+        @raw_data['datapoints'].delete(@raw_data['datapoints'].last) if @raw_data['datapoints'].last.first.nil?
         @data = @raw_data['datapoints'].map(&:first)
         @target = @raw_data['target']
         @start = @raw_data['datapoints'].first.last
         @end = @raw_data['datapoints'].last.last
         @step = ((@end - @start) / @raw_data['datapoints'].size.to_f).ceil
+
+        @data.map! { |val| val || 0 }
         nil
-      rescue OpenURI::HTTPError
+      rescue
         critical "Failed to connect to graphite server"
-      rescue NoMethodError
-        critical "No data for time period and/or target"
       end
     end
   end
@@ -117,18 +118,20 @@ class CheckGraphiteData < Sensu::Plugin::Check::CLI
   # type:: :warning or :critical
   # Return alert if required
   def check(type)
-    if config[type]
-      if @data.last > config[type] && !decreased?
+    cmp = config[:comparator]
+    if(config[type])
+      if(@data.last.send(cmp, config[type]) && !changed?(cmp))
         send(type, "#{name} has passed #{type} threshold")
       end
     end
   end
 
-  # Check if values have decreased within interval if given
-  def decreased?
-    if config[:reset_on_decrease]
-      slice = @data.slice(@data.size - config[:reset_on_decrease], @data.size)
-      val = slice.shift until slice.empty? || val.to_f > slice.first
+  def changed?(op)
+    cfg = config[:reset_on_change]
+
+    if(cfg)
+      slice = @data.slice(@data.size - cfg, @data.size)
+      val = slice.shift until slice.empty? || val.to_f.send(op, slice.first)
       !slice.empty?
     else
       false
@@ -137,7 +140,7 @@ class CheckGraphiteData < Sensu::Plugin::Check::CLI
 
   # Returns formatted target with hostname replacing any $ characters
   def formatted_target
-    if config[:target].include?('$')
+    if(config[:target].include?('$'))
       require 'socket'
       @formatted = Socket.gethostbyname(Socket.gethostname).first.gsub('.', config[:hostname_sub] || '_')
       config[:target].gsub('$', @formatted)
